df9b9c660abc487275b11cb87ffe9d22d317891e
gody <gody84@gmail.com>
2019-05-08 20:00:17 -0400

    Primera clase jQuery

---
+++ Clase 15 y 16/first_javascript.js
@@ -0,0 +1,13 @@
+var miTitulo = document.querySelector('div');
+miTitulo.innerHTML = '<h1>Hola Clase</h1> <h2 class="hola">Subtitulo</h2>';
+
+/* Comentarios de varias lineas
+
+var miTitulo, crear una "Variable"
+
+document selecciona la página completa
+.querySelector('h1') selecciona el primer h1 de la página
+<h1></h1>
+.innerHTML que al ser '=' estamos asignando el valor HOLA MUNDO! al interior del selector
+
+*/
